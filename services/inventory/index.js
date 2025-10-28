b3ece6b72eaa9a8931197f6d71be4e31f8b6554e
v-n-s <133883699+v-n-s@users.noreply.github.com>
2025-10-28 18:51:05 -0300

    Implement SearchProductByID in InventoryService
    Added SearchProductByID method to InventoryService.

--- services/inventory/index.js
+++ services/inventory/index.js
@@ -2,6 +2,7 @@ const grpc = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
 const products = require('./products.json');
 
+// Carrega o arquivo .proto
 const packageDefinition = protoLoader.loadSync('proto/inventory.proto', {
     keepCase: true,
     longs: String,
@@ -11,17 +12,26 @@ const packageDefinition = protoLoader.loadSync('proto/inventory.proto', {
 
 const inventoryProto = grpc.loadPackageDefinition(packageDefinition);
 
+// Cria o servidor gRPC
 const server = new grpc.Server();
 
-// implementa os métodos do InventoryService
+// Implementa os métodos do InventoryService
 server.addService(inventoryProto.InventoryService.service, {
-    searchAllProducts: (_, callback) => {
+    // Retorna todos os produtos
+    SearchAllProducts: (_, callback) => {
         callback(null, {
             products: products,
         });
     },
+
+    // 🔹 Nova função adicionada (Tarefa Prática #1)
+    SearchProductByID: (payload, callback) => {
+        const product = products.find((p) => p.id == payload.request.id);
+        callback(null, product);
+    },
 });
 
+// Inicializa o servidor
 server.bindAsync('127.0.0.1:3002', grpc.ServerCredentials.createInsecure(), () => {
     console.log('Inventory Service running at http://127.0.0.1:3002');
     server.start();
